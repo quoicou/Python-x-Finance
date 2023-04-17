9f05720022ffc24863716095969a70110906b8c3
Fabien Dpy <72447511+de-la-salle-sio-dupuy-f@users.noreply.github.com>
2023-04-17 09:14:34 +0200

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,4 +1,3 @@
-import unidecode
 import requests
 import datetime
 from bs4 import BeautifulSoup
@@ -6,27 +5,23 @@ import re
 import os
 
 def start():
-    choix = 0
+    choix = int(input("\n##################\nQue souhaitez-vous ?\n\n1) Consulter action\n2) Comparer wallet\n3) Ajouter une action au wallet\n\nChoix : "))
 
     while choix not in [1, 2, 3]:
 
-        choix = int(input("Que souhaitez-vous ?\n1) Consulter action\n2) Comparer wallet\n3) Ajouter une action au wallet\nChoix : "))
+        choix = int(input("\n##################\nERREUR DE SAISIE\n\nQue souhaitez-vous ?\n\n1) Consulter action\n2) Comparer wallet\n3) Ajouter une action au wallet\nChoix : "))
 
-        if choix == 1:
-            return choix
-
-        elif choix == 2:
-            return choix
-
-        elif choix == 3:
-            return choix
+    if choix in [1, 2, 3]:
+        return choix
 
 def isin(choix):
+    code_isin = ""
+
     if choix == 1:
-        code_isin = input("Rentrez le code ISIN de l'action que vous souhaitez consulter\nChoix : ")
+        code_isin = input("\n##################\nRentrez le code ISIN de l'action que vous souhaitez consulter\n\nChoix : ")
 
     if choix == 3:
-        code_isin = input("Rentrez le code ISIN de l'action que vous souhaitez ajouter\nChoix : ")
+        code_isin = input("\n##################\nRentrez le code ISIN de l'action que vous souhaitez ajouter\n\nChoix : ")
 
     return code_isin
 
@@ -69,11 +64,11 @@ def scrapping(url_recup):
     return nom_action, price
 
 def write(nom_action, price, code_isin, url_recup):
-    nb_action = int(input("Combien d'action souhaitez-vous acheter ?\nChoix : "))
+    nb_action = int(input("\n##################\nCombien d'action souhaitez-vous acheter ?\n\nChoix : "))
 
     prix_total = round(price * nb_action, 4)
 
-    print(f"Vous avez acheté pour {prix_total} € d'action ")
+    print(f"\nVous avez acheté pour {prix_total} € d'action ")
 
     filename = "C://Users//fabi1//Documents//PYTHONxFinance//wallet.txt"
     with open(filename, "a") as f:
@@ -81,46 +76,66 @@ def write(nom_action, price, code_isin, url_recup):
 
 def read():
     chemin = "C://Users//fabi1//Documents//PYTHONxFinance//"
-    print("Voici tous les wallets que vous pouvez évaluer\nListe des wallets :")
-    fichiers = os.listdir(chemin)
+    print("\n##################\nVoici la liste de tous les wallets que vous pouvez évaluer : ")
 
-    # Affiche les noms des fichiers
-    for nom_fichier in fichiers:
-        print(nom_fichier)
-    nom_wallet = input("Quel est le nom du wallet que vous souhaitez évaluer ?\nChoix : ")
+    liste_wallet = os.listdir(chemin)
+
+    affichage_liste_wallet(liste_wallet)
+
+    nom_wallet = input("\nQuel est le nom du wallet que vous souhaitez évaluer ?\n\nChoix : ")
+
+    while nom_wallet not in liste_wallet:
+        print("\n##################\nERREUR DE SAISIE\n\nVoici la liste de tous les wallets que vous pouvez évaluer : ")
+
+        affichage_liste_wallet(liste_wallet)
+
+        nom_wallet = input("\nQuel est le nom du wallet que vous souhaitez évaluer ?\n\nChoix : ")
+
     filename = chemin + nom_wallet
 
     with open(filename, "r") as f:
         transactions = f.readlines()
     return transactions
 
+def affichage_liste_wallet(liste_wallet):
+    # Affiche les noms des fichiers
+    for nom_wallet in liste_wallet:
+        print(nom_wallet)
+
 def calcul_gain(transactions):
     #current_datetime = datetime.datetime.now()
     total_gain_loss = 0.0
-    for transaction in transactions:
-        match = re.search(r'\[\[(.*?)\]\]', transaction)
-        if match:
-            transaction_info = match.group(1).split('][')
-            prix_achat = float(transaction_info[2])
-            quantite = int(transaction_info[3])
-            url = transaction_info[5]
+    try:
+        for transaction in transactions:
+            match = re.search(r'\[\[(.*?)\]\]', transaction)
+            if match:
+                transaction_info = match.group(1).split('][')
+                prix_achat = float(transaction_info[2])
+                quantite = int(transaction_info[3])
+                url = transaction_info[5]
 
-            nom_action, prix_actuel = scrapping(url)
+                nom_action, prix_actuel = scrapping(url)
 
-            gain_perte_unitaire = round(prix_achat - prix_actuel, 4)
+                gain_perte_unitaire = round(prix_achat - prix_actuel, 4)
 
-            if gain_perte_unitaire > 0:
-                print(f"L'action {nom_action} a une plus-value de {gain_perte_unitaire} €")
-            else:
-                print(f"L'action {nom_action} a une moins-value de {gain_perte_unitaire} €")
+                if gain_perte_unitaire > 0:
+                    print(f"L'action {nom_action} a une plus-value de {gain_perte_unitaire} €")
+                else:
+                    print(f"L'action {nom_action} a une moins-value de {gain_perte_unitaire} €")
 
-            total_gain_loss += gain_perte_unitaire * quantite
+                total_gain_loss += gain_perte_unitaire * quantite
 
-    if total_gain_loss > 0:
-        print(f"Le wallet a une plus-value totale de {round(total_gain_loss, 4)} €")
-    else:
-        print(f"Le wallet a une moins-value totale de {round(total_gain_loss, 4)} €")
+        if total_gain_loss > 0:
+            print(f"Le wallet a une plus-value totale de {round(total_gain_loss, 4)} €")
+        else:
+            print(f"Le wallet a une moins-value totale de {round(total_gain_loss, 4)} €")
 
+    except:
+        print("\n##################\nERREUR : Problème de connexion\n\nVeuillez à bien être connecté à internet")
+
+"""
+PARTIE PROCESSUS
+"""
 if __name__ == '__main__':
     choix = start()
 
@@ -140,5 +155,7 @@ if __name__ == '__main__':
         write(nom_action, price, code_isin, url_recup)
 
 """
-Display un tableau compte-rendu ou même graphique
+- Display un tableau compte-rendu ou même graphique pour wallet (modif affichage par action / mettre tableau ?)
+- Ajouter pour ETF
+- Créer wallet (while nom_wallet not in [liste_des_noms_des_wallet]
 """
